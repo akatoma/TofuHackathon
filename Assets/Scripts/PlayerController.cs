db74d2efa25e6a4ecd3d10e5bcbc330e9c8e7840
那須 <hinatahinatahinata3@gmail.com>
2026-08-24 00:37:18 +0900

    addPlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 720f;
+
+    Rigidbody rb;
+    Vector3 inputDirection = Vector3.zero;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        inputDirection = new Vector3(h, 0f, v);
+
+        if (inputDirection.sqrMagnitude > 0.0001f)
+        {
+            Quaternion target = Quaternion.LookRotation(inputDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, target, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = inputDirection.normalized * moveSpeed * Time.fixedDeltaTime;
+        if (rb != null)
+        {
+            rb.MovePosition(rb.position + move);
+        }
+        else
+        {
+            transform.Translate(move, Space.World);
+        }
+    }
+}
